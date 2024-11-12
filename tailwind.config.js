ef92f9c5fc51bfc65928021a9b3a2f5db32f358b
tanishktiwari <155141073+tanishktiwari@users.noreply.github.com>
2024-11-12 21:30:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customcolor: '#C1F11D',  // No trailing comma here
+        textcolor: '#141414',     // No trailing comma here
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],  // No trailing comma here
+      },
+      fontWeight: {
+        extraLight: 200,  // No trailing comma here
+        bold: 700,        // No trailing comma here
+      },
+    },
   },
   plugins: [],
 }
